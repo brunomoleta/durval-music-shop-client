4939a1ce0eb771b68fbaeb206a35907904122735
Bruno Moleta <brunomoleta@pm.me>
2024-03-21 13:12:54 -0300

    Move styled components to separate file
    The styled components for the Catalog component have been moved to a separate file, Catalog.styles.ts, to simplify the Catalog.tsx file. All related styling logic including media queries has also been shifted to this new file.

---
+++ src/styled-components/Catalog.styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+import {QUERIES} from "../services/database.ts";
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  gap: 2rem;
+`;
+
+export const ProductsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+
+  gap: 2.3rem;
+  padding: 0 2rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+
+    row-gap: 1.5rem;
+    column-gap: 2rem;
+  }
+
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
