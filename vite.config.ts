fe9124e371452f4c30117edfeaa43a982b58de3d
Bruno Moleta <brunomoleta@pm.me>
2024-03-21 16:37:40 -0300

    Vitest working 100% correctly
    Removed the `vitest.config.ts` file and integrated its configurations into `vite.config.ts`, updated `tsconfig.json` to include `vitest/globals`, and upgraded some dev dependencies in `package.json`. Added `vite-plugin-checker` to `vite.config.ts` and adjusted the file paths in the testing configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// < reference types="vitest" / >
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(()=> ({
+
+  plugins: [
+    react(),
+    checker({ typescript: true }),
+  ],
+  test:{
+    globals: true,
+    environment: "jsdom",
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: "./src/vitest.setup.ts"
+  },
+}))
