41135586955fc4929185d4bbb1521dfc0b8ec6cf
Bruno Moleta <brunomoleta@pm.me>
2024-03-21 13:14:53 -0300

    Introduce a separate CartButton component
    This commit refactors the CardProduct component by creating a separate CartButton component and incorporating it into CardProduct. This enhances code readability and modularity, making it easier to maintain.

---
+++ src/components/Cart/CartButton/CartButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { AddCartButton } from "../../../styled-components/Button.styles.ts";
+import { ProfileIcon } from "../../../styled-components/Header.styles.tsx";
+import { fontSize } from "../../../styled-components/root.ts";
+import Cart from "../../../assets/ui/Cart.svg";
+import { useCartContext } from "../../../providers/UserContext";
+import { ICartContext } from "../../../types/cart";
+import { CardProductProps } from "../../../types/product";
+
+type CardBtn = CardProductProps & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+function CartButton({ item, ...props }: CardBtn) {
+  const { addProductInCart } = useCartContext() as ICartContext;
+  return (
+    <AddCartButton
+      onClick={() => {
+        addProductInCart(item);
+      }}
+      {...props}
+        title="adicionar ao carrinho"
+    >
+      <ProfileIcon src={Cart} alt="Carrinho" />
+      <span style={{ fontSize: fontSize.icons }}>CARRINHO+</span>
+    </AddCartButton>
+  );
+}
+
+export default CartButton;
