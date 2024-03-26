46c7b3a5792e62732ff35dbdede1278d47df7431
Bruno Moleta <brunomoleta@pm.me>
2024-03-26 12:38:02 -0300

    Refactor user login and signup tests
    Updated the login and signup testing procedure to use fixtures for user data and validation messages to make tests more maintainable and consistent. This approach separates test data from test logic, improving test readability and ease of maintenance.

--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -1,50 +1,35 @@
-export const schemaMessages = {
-  firstName: "Favor coloque seu o nome :)",
-  lastName: "Favor coloque seu sobrenome :)",
-  email: "Por favor insira um e-mail válido",
-  noConfirmation: "Favor confirmar a senha.",
-  equalPassword: "As senhas hão de ser identicasíssimas.",
-  minimumPassword: "O tamanho mínimo é de 8 caracteres :)",
-};
-
 describe("user login", () => {
-  const typeUserInfo = () => {
-    cy.get('input[name="firstName"]').type(`${userSuccess.firstName}`);
-    cy.get('input[name="lastName"]').type(`${userSuccess.lastName}{enter}`);
-    cy.wait(200);
-
-    cy.get('input[name="email"]').type(`${userSuccess.email}{enter}`);
-    cy.wait(200);
-
-    cy.get('input[name="password"]').type(`${userSuccess.password}{enter}`);
-    cy.get('input[name="confirmPassword"]').type(
-      `${userSuccess.confirmPassword}{enter}`,
-    );
-    cy.wait(200);
-  };
-  const userSuccess = {
-    firstName: "Rulian",
-    lastName: "Ramirez",
-    email: "rulianramirez@gmail.com",
-    password: "aA0123!aaa",
-    confirmPassword: "aA0123!aaa",
-  };
-  const userFail = {
-    firstName: " ",
-    lastName: "   ",
-    email: "rulianramirezgmail.com",
-    password: "aA0123!aaa",
-    confirmPassword: "aA0123!aaaA",
-  };
-
   const buttonStrings = [/conta/i, /cadastrar/i, /enviar/i];
 
   beforeEach(() => {
+    cy.fixture("userFail").then((fail) => {
+      this.fail = fail;
+    });
+    cy.fixture("schemaMessages").then((messages) => {
+      this.messages = messages;
+    });
+    cy.fixture("userSuccess").then((success) => {
+      this.success = success;
+    });
     cy.visit("");
     cy.contains("button", buttonStrings[0]).click();
     cy.wait(200);
     cy.contains("button", buttonStrings[1]).click();
   });
+  const typeUserInfo = () => {
+    cy.get('input[name="firstName"]').type(`${this.success.firstName}`);
+    cy.get('input[name="lastName"]').type(`${this.success.lastName}{enter}`);
+    cy.wait(200);
+
+    cy.get('input[name="email"]').type(`${this.success.email}{enter}`);
+    cy.wait(200);
+
+    cy.get('input[name="password"]').type(`${this.success.password}{enter}`);
+    cy.get('input[name="confirmPassword"]').type(
+      `${this.success.password}{enter}`,
+    );
+    cy.wait(200);
+  };
 
   it("Should not be able to create a user successfully as the email already exists.", () => {
     typeUserInfo();
@@ -57,32 +42,32 @@ describe("user login", () => {
     cy.get("div.Toastify__toast--error");
   });
 
-  it.only("Should be able to navigate the dialog fully", () => {
-    cy.get('input[name="firstName"]').type(`${userFail.firstName}`);
-    cy.get('input[name="lastName"]').type(`${userFail.lastName}{enter}`);
+  it("Should be able to navigate the dialog fully", () => {
+    cy.get('input[name="firstName"]').type(`${this.fail.firstName}`);
+    cy.get('input[name="lastName"]').type(`${this.fail.lastName}{enter}`);
 
-    cy.get("span").should("contain.text", schemaMessages.firstName);
-    cy.get("span").should("contain.text", schemaMessages.lastName);
+    cy.get("span").should("contain.text", this.messages.firstName);
+    cy.get("span").should("contain.text", this.messages.lastName);
 
-    cy.get('input[name="firstName"]').clear().type(`${userSuccess.firstName}`);
+    cy.get('input[name="firstName"]').clear().type(`${this.success.firstName}`);
     cy.get('input[name="lastName"]')
       .clear()
-      .type(`${userSuccess.lastName}{enter}`);
+      .type(`${this.success.lastName}{enter}`);
     cy.wait(200);
 
-    cy.get('input[name="email"]').type(`${userFail.email}{enter}`);
-    cy.get("span").should("contain.text", schemaMessages.email);
-    cy.get('input[name="email"]').clear().type(`${userSuccess.email}{enter}`);
+    cy.get('input[name="email"]').type(`${this.fail.email}{enter}`);
+    cy.get("span").should("contain.text", this.messages.email);
+    cy.get('input[name="email"]').clear().type(`${this.success.email}{enter}`);
 
-    cy.get('input[name="password"]').type(`${userFail.password}{enter}`);
-    cy.get("span").should("contain.text", schemaMessages.noConfirmation);
+    cy.get('input[name="password"]').type(`${this.fail.password}{enter}`);
+    cy.get("span").should("contain.text", this.messages.noConfirmation);
     cy.get('input[name="confirmPassword"]').type(
-      `${userFail.confirmPassword}{enter}`,
+      `${this.fail.confirmPassword}{enter}`,
     );
-    cy.get("span").should("contain.text", schemaMessages.equalPassword);
+    cy.get("span").should("contain.text", this.messages.equalPassword);
     cy.get('input[name="confirmPassword"]')
       .clear()
-      .type(`${userFail.password}{enter}`);
+      .type(`${this.fail.password}{enter}`);
 
     cy.get("button").should("contain.text", "LOGIN");
     cy.get("button").should("contain.text", "ENVIAR");
@@ -91,6 +76,6 @@ describe("user login", () => {
     typeUserInfo();
 
     cy.contains("button", "LOGIN").click();
-    cy.contains("h2",/login/i);
+    cy.contains("h2", /login/i);
   });
 });
