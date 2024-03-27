d156636045d3cd827c8266073844e1a091b315a8
Bruno Moleta <brunomoleta@pm.me>
2024-03-27 16:13:01 -0300

    Improve RenderProducts component's functionality
    The RenderProducts component has been expanded to handle user loading status and added optional pagination to the products list.

--- src/components/RenderProducts/RenderProducts.tsx
+++ src/components/RenderProducts/RenderProducts.tsx
@@ -3,23 +3,50 @@ import Illustration from "../Illustration";
 import NoOrder from "../../assets/illustrations/No-Order.svg";
 import ProductsList from "../AllProducts/ProductsList";
 import ChangeProductPage from "../AllProducts/ChangeProductPage";
-import {IProductContext} from "../../types/product";
+import { IProductContext } from "../../types/product";
+import { useUserContext } from "../../providers/UserContext";
+import { IUserContext } from "../../types/user";
+import Loader from "../Loader";
 
-function RenderProducts({products}: { products: IProductContext[] }) {
+function RenderProducts({
+  products,
+  hasPagination = true,
+}: {
+  products: IProductContext[];
+  hasPagination?: boolean;
+}) {
+  const { isLoading } = useUserContext() as IUserContext;
+  const renderAnswer = () => {
+    if (isLoading) <Loader />;
+
+    else if (!products) {
+      return (
+        <NoProductFound
+          element={<Illustration image={NoOrder} alt="" />}
+          message="Infelizmente não foi possível mostrar-te os produtos :("
+          subTitle="Você sabia que nós somos o e-commerce nº01 no Brasil no ReclameAqui?"
+          isButton={false}
+        />
+      );
+    } else if (products.length === 0) {
+      return (
+        <NoProductFound
+          element={<Illustration image={NoOrder} alt="" />}
+          message="Infelizmente não há produtos para sua busca :("
+          subTitle="Você sabia que nós somos o e-commerce nº01 no Brasil no ReclameAqui?"
+          isButton={false}
+        />
+      );
+    }
     return (
-        !products ? (
-            <NoProductFound
-                element={<Illustration image={NoOrder} alt=""/>}
-                message="Infelizmente não foi possível trazer os produtos :("
-                subTitle="Você sabia que nós somos o e-commerce nº01 no Brasil no ReclameAqui?"
-                isButton={false}
-            />
-        ) : (
-            <>
-                <ProductsList products={products}/>
-                <ChangeProductPage/>
-            </>
-        )
+      <>
+        <ProductsList products={products} />
+        {hasPagination && <ChangeProductPage />}
+      </>
     );
+  };
+
+  return renderAnswer();
 }
+
 export default RenderProducts;
