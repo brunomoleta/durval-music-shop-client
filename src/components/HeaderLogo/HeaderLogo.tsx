5c83823b73eb6e29d83fc9746350f7b7ee3ac423
Bruno Moleta <brunomoleta@pm.me>
2024-03-27 16:10:35 -0300

    Add HeaderLogo component and implement return home function
    This commit introduces a new HeaderLogo component to enhance reusability and simplify the HeaderInfo structure. It also adds a "returnHome" function in the ProductContext, allowing redirection to the homepage and clean-up of the search field.

---
+++ src/components/HeaderLogo/HeaderLogo.tsx
@@ -0,0 +1,15 @@
+import { LogoTop } from "../../styled-components/Header.styles.tsx";
+import { logoName } from "../../services/database.ts";
+import { useProductContext } from "../../providers/UserContext";
+import { IFullProductContext } from "../../types/product";
+
+function HeaderLogo() {
+  const { returnHome } = useProductContext() as IFullProductContext;
+  return (
+    <button onClick={returnHome}>
+      <LogoTop>{logoName}</LogoTop>
+    </button>
+  );
+}
+
+export default HeaderLogo;
