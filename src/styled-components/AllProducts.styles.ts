392ce96c75667038f4d499e0a7ba310582eacfa4
Bruno Moleta <brunomoleta@pm.me>
2024-03-30 11:55:01 -0300

    Update AllProducts.styles.ts styling properties
    Adjusted spacing for code readability and added 'width: fit-content' to the Heading styled component in AllProducts.styles.ts.

--- src/styled-components/AllProducts.styles.ts
+++ src/styled-components/AllProducts.styles.ts
@@ -2,17 +2,18 @@ import styled from "styled-components";
 import { colors, fontSize, genericValues } from "./root.ts";
 
 export const Heading = styled.h2`
-  font-size: clamp(${fontSize.smallLink}, 7svw, ${fontSize.h3});
-  font-weight: 600;
-  color: ${colors.purple};
+    font-size: clamp(${fontSize.smallLink}, 7svw, ${fontSize.h3});
+    font-weight: 600;
+    width: fit-content;
+    color: ${colors.purple};
 `;
 export const Wrapper = styled.section`
-  display: grid;
-  margin-block: 40px;
-  margin-inline: 16px;
-  padding-inline: ${genericValues.pagePadding};
-  align-items: self-start;
-  gap: 32px;
-  width: 100%;
-  position: relative;
+    display: grid;
+    margin-block: 40px;
+    margin-inline: 16px;
+    padding-inline: ${genericValues.pagePadding};
+    align-items: self-start;
+    gap: 32px;
+    width: 100%;
+    position: relative;
 `;
